dd00fa9638e6c409f1893bc10d0a205150d45902
David Palacios <davidpa7710@gmail.com>
2022-05-15 14:20:47 -0500

    el keypress funciona correctamente

--- src/index.js
+++ src/index.js
@@ -5,22 +5,38 @@ var formulario = document.querySelector('.formulario')
 var lista = document.querySelector('.list-contain')
 
 
-function validar () {
+function validar() {
     var userInput = input.value
-    if (emtpyText.textContent === '') {
-        emtpyText.textContent = `Usted ingreso el texto: ${userInput}`
-        console.log(' el elemento no esta ocupado');
-    } else {
-       var li = document.createElement('li')
-       li.textContent = `Usted ingreso otro texto: ${userInput}`
-       lista.insertAdjacentElement('afterbegin', li)
-       console.log(' el elemento esta ocupado se inserto en otro elemento');
+    if (userInput === '') {
+        alert('ingrese un texto por favor')
+    }
+    else {
+        if (emtpyText.textContent === '') {
+            emtpyText.textContent = `Usted ingreso el texto: ${userInput}`
+            console.log(' el elemento no esta ocupado');
+        } else {
+            var li = document.createElement('li')
+            li.textContent = `Usted ingreso otro texto: ${userInput}`
+            lista.insertAdjacentElement('afterbegin', li)
+            console.log(' el elemento esta ocupado se inserto en otro elemento');
+        }
+    }
+
+}
+function keyPress(e) {
+    if (e.keyCode == '13') {
+        e.preventDefault();
+        validar();
     }
 }
+
+window.addEventListener('keydown', keyPress)
+
+
+
 
 
 
 btn.addEventListener('click', validar)
 
 
-  
